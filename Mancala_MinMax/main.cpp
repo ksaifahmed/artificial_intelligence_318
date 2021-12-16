6572494c3c2f92c181a63b4a7dd7310e99eb5d9b
ksaifahmed <ksaifahmed@gmail.com>
2021-12-16 12:40:45 +0600

    minmax mancala, pruned_minmax function

--- Mancala_MinMax/main.cpp
+++ Mancala_MinMax/main.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+#define INF 2147480000
+#define MAX_DEPTH 10
 
 struct Board {
     int *pockets;
@@ -14,6 +16,7 @@ struct Board {
         pockets[0] = pockets[7] = 0;
     }
 };
+typedef vector<Board> BoardChild;
 
 
 Board getCopy(Board board) {
@@ -32,12 +35,12 @@ Board getMove(Board b, int index) {
     while (beads--) {
         if ((!top && !index) || index == -1) index = 13; // for going round and skipping opponent store
         else if (top && index == 7) index--; //skip opponents store
-        cout << "value: " << board.pockets[index] << ", index: " << index << ", beads, " << beads << endl;
+        //cout << "value: " << board.pockets[index] << ", index: " << index << ", beads, " << beads << endl;
         board.pockets[index]++;
 
         //steal
-        if(!beads && board.pockets[index] == 1) {
-            cout << "index: " << 14-index << ", val: " << board.pockets[14-index] << endl;
+        if(!beads && board.pockets[index] == 1 && index != 0 & index != 7) {
+            //cout << "index: " << 14-index << ", val: " << board.pockets[14-index] << endl;
             if(top) board.pockets[0] += board.pockets[14-index] + 1;
             else board.pockets[7] += board.pockets[14-index] + 1;
             board.pockets[14-index] = board.pockets[index] = 0;
@@ -46,6 +49,24 @@ Board getMove(Board b, int index) {
         index--;
     }
     return board;
+}
+
+
+BoardChild getChildren(Board board, bool top){
+    BoardChild list;
+    if(top){
+        for(int i=0; i<7; i++){
+            if(board.pockets[i])
+                list.push_back(getMove(board, i));
+        }
+
+    }else{
+        for(int i=13; i>7; i--){
+            if(board.pockets[i])
+                list.push_back(getMove(board, i));
+        }
+    }
+    return list;
 }
 
 
@@ -66,10 +87,58 @@ void printBoard(Board board) {
 }
 
 
+
+typedef pair<Board, int> ScoreBoard;
+ScoreBoard pruned_minmax(Board board, int depth, bool is_max, int alpha, int beta) {
+    //leaf
+    if(end_of_game(board)) {
+        if(board.pockets[0] > board.pockets[7])
+            return make_pair(board, board.pockets[0]+INF);
+        else return make_pair(board, -(board.pockets[7]+INF));
+    }
+
+    //max_depth
+    if(depth == MAX_DEPTH) {
+        return make_pair(board, board.pockets[7] - board.pockets[0]);
+    }
+
+    if(is_max){
+        int best_score = -INF;
+        BoardChild list = getChildren(board, true); //gets children of top(first) player
+        for(auto child : list) {
+            int score = pruned_minmax(child, depth+1, false, alpha, beta).second;
+            best_score = max(score, best_score);
+            if(best_score >= beta) return make_pair(board, best_score);
+            alpha = max(alpha, best_score);
+        }
+        return make_pair(board, best_score);
+
+    }else{
+        int best_score = INF;
+        BoardChild list = getChildren(board, false); //gets children of bottom(second) player
+        for(auto child : list){
+            int score = pruned_minmax(child, depth+1, true, alpha, beta).second;
+            best_score = min(score, best_score);
+            if(best_score <= alpha) return make_pair(board, best_score);
+            beta = min(beta, best_score);
+        }
+        return make_pair(board, best_score);
+    }
+}
+
+
+
+
 int main() {
-    Board b, copy;
-    b.pockets[13] = 34;
-    //copy = b.getCopy();
-    printBoard(copy);
+    Board b;
+    b.init_board();
+
+    cout << "best: " << pruned_minmax(b, 0, true, -INF, INF).second << endl;
+
+    BoardChild list = getChildren(b, true);
+    for(auto child : list) {
+        cout << "score is : " << pruned_minmax(child, 1, false, -INF, INF).second << endl;
+    }
+    //printBoard(getMove(b, 4));
     return 0;
 }
